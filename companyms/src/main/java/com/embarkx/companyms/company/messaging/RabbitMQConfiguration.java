c56610c1b9615a0efe621570fa62502a7024bac1
Ahsan Zamil <ahsanzamil76@gmail.com>
2025-07-16 12:53:10 +0530

    updated RabbitMQConfiguration

--- companyms/src/main/java/com/embarkx/companyms/company/messaging/RabbitMQConfiguration.java
+++ companyms/src/main/java/com/embarkx/companyms/company/messaging/RabbitMQConfiguration.java
@@ -1,4 +1,4 @@
-package com.embarkx.companyms.messaging;
+package com.embarkx.companyms.company.messaging;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
